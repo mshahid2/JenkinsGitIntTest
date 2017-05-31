336a8e578ff1c4d791e853a8cd7f1be42c3dfb2b
Shahid <Owner@10.1.10.181>
2017-05-31 14:23:30 -0400

    Jenkins git integration Testing.

---
+++ JenkinsTest/src/com/xav/JenkinsGitIntTest.java
@@ -0,0 +1,13 @@
+package com.xav;
+
+public class JenkinsGitIntTest {
+	
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("I am printing it :" + i);
+		}
+	}
+
+}
